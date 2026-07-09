d78ab5114df75e534a1b61a6f960add9f182787f
thainarakerdy <thainarakerdy@gmail.com>
2026-07-08 21:56:35 -0300

    Initial commit

---
+++ primeiroo.py
@@ -0,0 +1,13 @@
+print('hello word!');
+print('Escola de Dados da Alura!')
+print('Nome: Thainara')
+print('Sobrenome: Kerdy')
+print( 2, 'Abril', 2004); #Para usar um comentário é necessario colocar "#" e para finalizar também é necessário colocar #
+'''
+teste de comentários 
+mais longos
+de várias linhas
+'''
+
+idade = 5
+print(idade);
