128c2aac1b62f8245096e024c729901456b1b651
thainarakerdy <thainarakerdy@gmail.com>
2026-07-10 22:35:41 -0300

    10/07
    aprendendo mais sobre as variáveis em Python

---
+++ descomplica_dev/variaveis_e_tipos_de_dados.py
@@ -0,0 +1,30 @@
+# idade = 23 # inteiro
+# print(idade)
+# print(type(idade))
+
+# altura = 1.75 # float
+# print(altura)
+# print(type(altura))
+
+# nome = "Maria" # String (str)
+# print(nome)
+# print(type(nome))
+
+# estudando = False
+# print("Está estudando?, estudando, "| Tipo:", type(estudando))
+
+# frutas = ["maçã", "banana", "laranja"] # List = podemos mudar a ordem e mudar os valores, retirar e adicionar
+# print(frutas)
+# print(type(frutas))
+
+# cores = ("vermelho", "azul", "marsala") # Tupla (tuple) é parecido com o "List" pórem não pode ser alterado, ou seja, não pode adicionar e muito menos mudar o que foi colocadoo até o final do código
+# print(cores)
+# print(type(cores))
+
+# aluno = {"nome": "Maria", "idade": 25, "curso": "Engenharia"} # Dicionário (dict) é uma váriavel com várias outras váriaveis dentro
+# print(aluno)
+# print(type(aluno))
+
+# numeros = {1, 2, 3, 3, 2, 1} # conjunto (set) é uma coleção não ordenada de itens únicos, ou seja, não repete os números que foram ccolocados mais de uma vez
+# print(numeros)
+# print(type(numeros))
