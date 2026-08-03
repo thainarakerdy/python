156a2d4d115cc0276596728ee3df8daf76df1fe8
thainarakerdy <thainarakerdy@gmail.com>
2026-08-02 23:02:24 -0300

    Update jogo_forca_atualizado.py

--- descomplica_dev/testes/jogo_forca_atualizado.py
+++ descomplica_dev/testes/jogo_forca_atualizado.py
@@ -26,4 +26,28 @@ def jogar():
     print("Tente adivinhar a palavra secreta")
 
 while True:
-    mostrar_estado(letras_descorbertas, erros, max_erros )
+    mostrar_estado(letras_descobertas, erros, max_erros)
+    letra = input("\nDigite uma letra: ").lower()
+
+    if letra in letras_descobertas or letra in erros:
+        print("Você já digitou essa letra!")
+
+    if letra in palavra_secreta:
+        print(f"Boa! Você acertou a letra {letra} está na palavra")
+        for i, char in enumerate(palavra_secreta):
+            if char == letra:
+                letras_descobertas[i] = letra
+    else:
+        print(f"A letra {letra} não está na palavra")
+        erros.append(letra)
+
+    if verificar_vitoria(letras_descobertas):
+        print("\nParabéns! Você acertou. A palavra era: ", palavra_secreta)
+        break
+
+    if len(erros) >= max_erros:
+        print("\n Game Over! Obrigado por jogar")
+        print("A palavra secreta era: ", palavra_secreta)
+
+if __name__ == "__jogo_forca_atualizado__":
+    jogar()       
